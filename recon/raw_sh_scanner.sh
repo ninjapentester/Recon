86adfb335e36ade2d93b2b2edca681fdae822286
matt <ninjapentester@gmail.com>
2021-08-11 08:19:07 -0400

    Added raw_sh_scanner.sh

---
+++ recon/raw_sh_scanner.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+################### DESCRIPTION #########################################
+# Raw bash port scanner leveraging /dev/tcp, created by Mattia Campagnano
+# (The_S@vvy_G33k) on Wed 11 Aug 2021 08:08:56 AM EDT
+
+# Originally published by Mattia Campanano as LOTL_scanner.sh (https://github.com/matticamp/LOTL_scanner/blob/main/LOTL_scanner.sh)
+# It will read in a list of IP addresses from a user-provided text file
+# and will use /dev/tcp to scan ports 1 thru 65,535 for each target IPs.
+
+#########################################################################
+
+# Enter path to a file containing a list of IP addresses and store it into a variable
+echo -n "Enter path to IP list: "
+read IP_list
+
+#Creating a variable storing the value of the IP_list variable (the file path)
+base=$(echo $IP_list)
+
+# Loop through all ports
+for port in {1..65535}; do          
+
+# Loop through all IP addresses stored in the file whose path is stored in the base variable
+	for ip in $(cat $base);	do 
+	
+# For each IP address, connect to all 65,535 ports, discard errors and indicate
+# which port is open for which IP
+
+		timeout .1 bash -c "echo >/dev/tcp/$ip/$port" 2>/dev/null &&
+        echo "port $port is open for $ip"
+	done	
+done
+
+# Print "done" on screen when scan is completed.
+echo "Done"
